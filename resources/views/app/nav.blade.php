11839a07ca38293fcc84d8e4ba6357d91450516f
edilson67816051 <serranoluizedil@gmail.com>
2023-09-11 20:35:53 -0400

    Primera Subida

---
+++ resources/views/app/nav.blade.php
@@ -0,0 +1,33 @@
+<!--header-->
+<header class="main-header clearfix" role="header">
+    <div class="logo">
+      <a href="#"><em>M.</em> CHAMPAGNAT</a>
+    </div>
+    <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+    <nav id="menu" class="main-nav" role="navigation">
+      <ul class="main-menu">
+        <li><a href="{{url('/')}}">Home</a></li>
+        <li><a href="{{url('/historia')}}">Historia</a></li>
+        <li><a href="{{url('/proyectos')}}">Proyectos</a></li>
+        
+        <li class="has-submenu"><a href="#section2">ACERCA DE</a>
+          <ul class="sub-menu">
+            <li><a href="{{url('/historia')}}">Pagina Facebook</a></li>
+
+            <li><a href="#section3">Telefono</a></li>
+            <li><a href="#section3">Administracion</a></li>
+          </ul>
+        </li>
+        <li class="has-submenu"><a href="#section2">INDICE</a>
+          <ul class="sub-menu">
+            <li><a href="#section2">Festividades</a></li>
+            <li><a href="#section3">Acto Escolares</a></li>
+            <li><a href="#section3">Campamento</a></li>
+            <li><a href="#section3">Trabajo</a></li>
+        
+          </ul>
+        </li>
+        <li><a href="#section6">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
